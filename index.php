bfe0f0be69eff1bf1c89d7dc997a717ffef614df
Dien Vu <meotimdihia@gmail.com>
2012-09-27 23:05:28 +0700

    fixxed

--- index.php
+++ index.php
@@ -5,24 +5,39 @@
 <body>
 	<div id='result'></div>
 	<div id='output'></div>
+	<input type='text' id='entry' />
 	<script type='text/javascript'>
-	// $(function(){
+	$(function(){
 		var host = 'http://localhost:1337/';
 	// 	$.get(host, function(data){
 	// 		$('#result').html(data);
 	// 	});
 		
-	// })
-	var counter = 0;
-	var poll = function() {
-	$.getJSON(host + 'poll/'+counter, function(response) {
-		counter = response.count;
-		var elem = $('#output');
-		elem.text(elem.text() + response.append);
+	
+	function send(msg){
+  		$.get(host + 'msg/' + escape(msg), function(data){});
+  	}
+
+
+  	$("#entry").keypress(function(e){
+	    if (e.keyCode != 13 /* Return */) return;
+	    var msg = $("#entry").attr("value").replace("\n", "");
+	    send(msg);
+	    $("#entry").attr("value", ""); // clear the entry field.
+		return false;
+  	});
+
+		var counter = 0;
+		var poll = function() {
+		$.getJSON(host + 'poll/'+counter, function(response) {
+			counter = response.count;
+			var elem = $('#output');
+			elem.text(elem.text() + response.append);
+			poll();
+		});
+		}
 		poll();
-	});
-	}
-	poll();
+	})
 	</script>
 </body>
 </html>
--- message.js
+++ message.js
@@ -12,29 +12,29 @@ http.createServer(function (req, res) {
 	var url_parts = url.parse(req.url);
 	console.log(url_parts);
 	if(url_parts.pathname.substr(0, 5) == '/poll') {
-		var count = url_parts.pathname.replace(/[^0-9]*/, '');
-		console.log(count);
-		if(messages.length > count) {
-			res.end(JSON.stringify( {
-				count: messages.length,
-				append: messages.slice(count).join("\n")+"\n"
-			}));
-		} else {
-			clients.push(res);
-		}
+    		var count = url_parts.pathname.replace(/[^0-9]*/, '');
+    		console.log(count);
+    		if(messages.length > count) {
+    			res.end(JSON.stringify( {
+    				count: messages.length,
+    				append: messages.slice(count).join("\n")+"\n"
+    			}));
+    		} else {
+    			clients.push(res);
+    		}
   	} else if(url_parts.pathname.substr(0, 5) == '/msg/') {
-  // message receiving
-  var msg = unescape(url_parts.pathname.substr(5));
-  messages.push(msg);
-  while(clients.length > 0) {
-    var client = clients.pop();
-    client.end(JSON.stringify( {
-      count: messages.length,
-      append: msg+"\n"
-    }));
-  }
-  res.end();
-}
+        // message receiving
+        var msg = unescape(url_parts.pathname.substr(5));
+        messages.push(msg);
+        while(clients.length > 0) {
+          var client = clients.pop();
+          client.end(JSON.stringify( {
+            count: messages.length,
+            append: msg+"\n"
+          }));
+        }
+        res.end();
+    }
 
 }).listen(1337, 'localhost');
 console.log('Server running.');
